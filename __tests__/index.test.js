c4eff77d4ea473c0e462dc6ff439382bbe55bf43
ZihaoBAO <zihao.bao@outlook.com>
2025-06-11 11:30:55 +0200

    Add square function and test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,1 +1,2 @@
 "const add = require('../index'); test('adds 1 + 2 to equal 3', () => { expect(add(1, 2)).toBe(3); });" 
+const { square } = require('../index'); test('square 3 to be 9', () => { expect(square(3)).toBe(9); });
